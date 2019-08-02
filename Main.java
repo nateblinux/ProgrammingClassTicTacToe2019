ed0ed599848f7f1dfb6f138ef02eda68cf25cd7b
nateblinux <nateblinux@gmail.com>
2019-08-01 20:47:02 -0400

    Third Commit

--- Main.java
+++ Main.java
@@ -18,10 +18,48 @@ public class Main{
         return true;
     }
 
+    public static boolean checkForWin(){
+        if(board[0] == "X" && board[1] == "X" && board[2] == "X")
+            return false;
+        if(board[3] == "X" && board[4] == "X" && board[5] == "X")
+            return false;
+        if(board[6] == "X" && board[7] == "X" && board[8] == "X")
+            return false;
+        if(board[0] == "X" && board[3] == "X" && board[6] == "X")
+            return false;
+        if(board[1] == "X" && board[4] == "X" && board[7] == "X")
+            return false;
+        if(board[2] == "X" && board[5] == "X" && board[8] == "X")
+            return false;
+        if(board[0] == "X" && board[4] == "X" && board[8] == "X")
+            return false;
+        if(board[2] == "X" && board[4] == "X" && board[6] == "X")
+            return false;
+        
+        if(board[0] == "O" && board[1] == "O" && board[2] == "O")
+            return false;
+        if(board[3] == "O" && board[4] == "O" && board[5] == "O")
+            return false;
+        if(board[6] == "O" && board[7] == "O" && board[8] == "O")
+            return false;
+        if(board[0] == "O" && board[3] == "O" && board[6] == "O")
+            return false;
+        if(board[1] == "O" && board[4] == "O" && board[7] == "O")
+            return false;
+        if(board[2] == "O" && board[5] == "O" && board[8] == "O")
+            return false;
+        if(board[0] == "O" && board[4] == "O" && board[8] == "O")
+            return false;
+        if(board[2] == "O" && board[4] == "O" && board[6] == "O")
+            return false;
+        
+        return true;
+    }
+
     static String player = "X";
     public static void main(String[] args){
         int i = 0;
-        while(i < 9){
+        while(i < 9 && checkForWin()){
             printBoard();
             System.out.println("where do you want to move? ");
             int userIn = input.nextInt();
@@ -38,5 +76,6 @@ public class Main{
                 System.out.println("space taken");
             }
         }
+        
     }
 }
